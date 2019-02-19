3f9e4cb6166bef9f639c18a4affd505fc7001e0c
Ivan <ivan.silva@multivarejogpa.com.br>
2019-02-18 21:32:16 -0300

    added GA

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,13 @@ class index extends React.Component{
         return (
             <div>
                 <Head>
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-54095242-1"></script>
+                    <script dangerouslySetInnerHTML= {{__html: `{window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+
+                        gtag('config', 'UA-54095242-1');}`}}/>
+                        <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`}} />                   
                     <title>Ivan Oliveira</title>
                     <link rel="shortcut icon" href="static/favicon.ico" />
                 </Head>
